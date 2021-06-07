32722a76c24af6933986538299655a69398ef74a
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2021-06-07 23:01:04 +0600

    Index Page Completed

---
+++ src/com/virtualkey/main/VirtualKeyMain.java
@@ -0,0 +1,14 @@
+package com.virtualkey.main;
+
+public class VirtualKeyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Index Page!! \n ");
+		System.out.println("Hi! I'm Ariful Islam, From Dhaka, Bangladesh \n "		       
+				+ "Welcome to my Lockedme.com Application. \n "
+				+ "This is a Document Management System.");
+		
+	}
+
+}
